9df871158642a2ec8bf3b5af38956af671583fe4
getfluxxed <derek@jondelconstruction.com>
2022-12-01 13:08:17 -0800

    fixed render bug and began rendering map

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // === ! CREATING THE VARIABLES AND SETTING UP CANVAS ! === \\
-const canvas = document.querySelector('canvas')
+const canvas = document.querySelector('#canvas')
 canvas.setAttribute('height', getComputedStyle(canvas)['height'])
 canvas.setAttribute('width', getComputedStyle(canvas)['width'])
 const ctx = canvas.getContext('2d')
@@ -7,12 +7,11 @@ const ctx = canvas.getContext('2d')
 
 // == ! CREATION OF ALL OBJECTS IN THE GAME ! == \\
 class Object {
-    constructor(x, y, width, height, name, color) {
+    constructor(x, y, width, height, color) {
         this.x = x
         this.y = y
         this.width = width
         this.height = height
-        this.name = name
         this.color = color
     }
     // == ! RENDERING OF OBJECTS METHOD ! == \\
@@ -22,37 +21,61 @@ class Object {
     }
 }
 
+// == ! Starting the Game ! == \\
+const gameRuntimeInterval = setInterval(gameRuntime, 60)
+
+// == ! Creation of Player and Mobs ! == \\
+const player = new Object(90, 55, 25, 25, 'lightgrey')
+const boss = new Object(1000, 200, 50, 76, 'white')
+
+// == ! Creation of Walls ! == \\
+const wallStart = new Object(200, 300, 35, -300, 'black')
+const wallBack = new Object(0, 0, 200, 35, 'black')
+const wallSide = new Object(0, 0, 35, 500)
+
+console.log(player, boss, wallStart)
 // == ! SETTING UP OF FUNCTIONS ! == \\
 // == ! Movement Function ! == \\
-function objectMovement(speed) {
 
-    if (pressedKeys.w) {
-        hero.y -= speed
-    }
-    if (pressedKeys.s) {
-        hero.y += speed
-    }
-    if (pressedKeys.a) {
-        hero.x -= speed
-    }
-    if (pressedKeys.d) {
-        hero.x += speed
-    }
-}
+// function objectMovement(speed) {
+//     if (pressedKeys.w) {
+//         hero.y -= speed
+//     }
+//     if (pressedKeys.s) {
+//         hero.y += speed
+//     }
+//     if (pressedKeys.a) {
+//         hero.x -= speed
+//     }
+//     if (pressedKeys.d) {
+//         hero.x += speed
+//     }
+// }
+
+// document.addEventListener('keydown', e => pressedKeys[e.key] = true)
+// document.addEventListener('keyup', e => pressedKeys[e.key] = false)
+
 // == ! Hit Detection Registration ! == \\
-function detectHit(objectOne, objectTwo) {
-    // == ! AXIS ALIGNED BOUNDING BOX COLLISION SYSTEM HIT DETECTION ! == \\
-    // == ! MAINLY CHECKING FOR OVERLAPS ! == \\
-    const left = objectOne.x + objectOne.width >= objectTwo.x
-    const right = objectOne.x <= objectTwo.x + objectTwo.width
-    const top = objectOne.y + objectOne.height >= objectTwo.y
-    const bottom = objectOne.y <= objectTwo.y + objectTwo.height
-    // console.log(left, right, top, bottom)
-    return left && right && top && bottom
-}
+// function detectHit(objectOne, objectTwo) {
+//     // == ! AXIS ALIGNED BOUNDING BOX COLLISION SYSTEM HIT DETECTION ! == \\
+//     // == ! MAINLY CHECKING FOR OVERLAPS ! == \\
+//     const left = objectOne.x + objectOne.width >= objectTwo.x
+//     const right = objectOne.x <= objectTwo.x + objectTwo.width
+//     const top = objectOne.y + objectOne.height >= objectTwo.y
+//     const bottom = objectOne.y <= objectTwo.y + objectTwo.height
+//     // console.log(left, right, top, bottom)
+//     return left && right && top && bottom
+// }
 
 // == ! GAMELOOP LOGIC ! == \\
 function gameRuntime() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // == ! Rendering of Objects ! == \\
+    wallStart.render()
+    player.render()
+    boss.render()
+    wallBack.render()
+    wallSide.render()
+
 }
 
