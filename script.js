a054ae919bf229087449acb2e49c2e04782fdd57
getfluxxed <derek@jondelconstruction.com>
2022-12-01 11:34:20 -0800

    beginning to tinker with creating Sprite images for character models

--- script.js
+++ script.js
@@ -15,9 +15,91 @@ class Object {
         this.name = name
         this.color = color
     }
+    // == ! RENDERING OF OBJECTS METHOD ! == \\
     render() {
         ctx.fillStyle = this.color
         ctx.fillRect(this.x, this.y, this.width, this.height)
     }
+}
+// == ! CREATION OF SPRITE MODELS FOR CHARACTERS ! == \\
+class Sprite {
+    constructor(options) {
+        this.context = options.context;
+        this.image = options.image; // Path to image sprite sheet
+        this.x = options.x; // Coordinates on canvas
+        this.y = options.y;
+        this.width = options.width; // Size of sprite frame
+        this.height = options.height;
+        this.frames = options.frames; // Number of frames in a row
+        this.frameIndex = options.frameIndex; // Current frame
+        this.row = options.row; // Row of sprites
+        this.ticksPerFrame = options.ticksPerFrame; // Speed of animation
+        this.tickCount = options.tickCount; // How much time has passed
+    }
+
+    update() {
+        this.tickCount += 1;
+        if (this.tickCount > this.ticksPerFrame) {
+            this.tickCount = 0;
+            if (this.frameIndex < this.frames - 1) {
+                this.frameIndex += 1;
+            } else {
+                this.frameIndex = 0;
+            }
+        }
+    }
+
+    render() {
+        this.context.drawImage(
+            this.image,
+            this.frameIndex * this.width, // The x-axis coordinate of the top left corner
+            this.row * this.height, // The y-axis coordinate of the top left corner
+            this.width, // The width of the sub-rectangle
+            this.height, // The height of the sub-rectangle
+            this.x, // The x coordinate
+            this.y,// The y coordinate
+            this.width, // The width to draw the image
+            this.height // The width to draw the image
+        );
+    }
+}
+
+
+
+
+
+
+// == ! SETTING UP OF FUNCTIONS ! == \\
+// == ! Movement Function ! == \\
+function objectMovement(speed) {
+
+    if (pressedKeys.w) {
+        hero.y -= speed
+    }
+    if (pressedKeys.s) {
+        hero.y += speed
+    }
+    if (pressedKeys.a) {
+        hero.x -= speed
+    }
+    if (pressedKeys.d) {
+        hero.x += speed
+    }
+}
+// == ! Hit Detection Registration ! == \\
+function detectHit(objectOne, objectTwo) {
+    // == ! AXIS ALIGNED BOUNDING BOX COLLISION SYSTEM HIT DETECTION ! == \\
+    // == ! MAINLY CHECKING FOR OVERLAPS ! == \\
+    const left = objectOne.x + objectOne.width >= objectTwo.x
+    const right = objectOne.x <= objectTwo.x + objectTwo.width
+    const top = objectOne.y + objectOne.height >= objectTwo.y
+    const bottom = objectOne.y <= objectTwo.y + objectTwo.height
+    // console.log(left, right, top, bottom)
+    return left && right && top && bottom
+}
+
+// == ! GAMELOOP LOGIC ! == \\
+function gameRuntime() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
 }
 
