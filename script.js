35f33ec2ef87b2c8b1755ca2f3f540aa8e6124df
getfluxxed <derek@jondelconstruction.com>
2022-12-02 14:12:07 -0800

    added a functional reset capability

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ const walkway = new Environment(400, 400, 500, 55, 'brown')
 const treasureChest = new Environment(90, 855, 50, 35, 'gold')
 const keyChest = new Environment(35, 395, 50, 35, 'gold')
 const doorStart = new Environment(200, 300, 35, 130, 'brown')
-const doorChest = new Environment(200, 465, 35, 135, 'brown')
+const swordChest = new Environment(200, 465, 35, 135, 'brown')
 
 // == ! Creation of Text Elements ! == \\
 const objective = new Text(450, 50, 'You gotta get out')
@@ -103,6 +103,7 @@ const lavaTxt = new Text(515, 50, 'Mmm Cwispy')
 const wonTxt = new Text(515, 50, 'Hey you won')
 const swordTxt = new SmallText(1050, 825, 'You have a Sword')
 const keyTxt = new SmallText(1050, 875, 'You have a Key')
+const directTxt = new Text(330, 100, 'WASD to move, WASD + C to dash. R to restart')
 // const deathText = new Text()
 
 
@@ -132,23 +133,27 @@ function characterMovement(speed) {
         }
         if (pressedKeys.w) {
             player.y -= speed
+            directTxt.display = false
         }
         if (pressedKeys.s && pressedKeys.c)
             player.y += speed + 50
         if (pressedKeys.s) {
             player.y += speed
+            directTxt.display = false
         }
         if (pressedKeys.a && pressedKeys.c) {
             player.x -= speed + 50
         }
         if (pressedKeys.a) {
             player.x -= speed
+            directTxt.display = false
         }
         if (pressedKeys.d && pressedKeys.c) {
             player.x += speed + 50
         }
         if (pressedKeys.d) {
             player.x += speed
+            directTxt.display = false
         }
         if (pressedKeys.i) {
             openLog()
@@ -156,11 +161,30 @@ function characterMovement(speed) {
         if (!pressedKeys.i) {
             closeLog()
         }
+        if (pressedKeys.r || !gameIsRunning) {
+            gameIsRunning = true
+            player.x = 90
+            player.y = 55
+            player.alive = true
+            player.hasKey = false
+            player.weapon = false
+            treasureChest.alive = true
+            keyChest.alive = true
+            boss.alive = true
+            hint.display = false
+            objective.display = true
+            deathTxt.display = false
+            lavaTxt.display = false
+            wonTxt.display = false
+            console.log('r')
+        }
+
     }
 }
 
 document.addEventListener('keydown', e => pressedKeys[e.key] = true)
 document.addEventListener('keyup', e => pressedKeys[e.key] = false)
+
 
 // == ! Hit Detection Registration ! == \\
 function detectHit(objectOne, objectTwo) {
@@ -174,9 +198,141 @@ function detectHit(objectOne, objectTwo) {
     return left && right && top && bottom
 }
 objective.display = true
+directTxt.display = true
 // == ! GAMELOOP LOGIC ! == \\
 function gameRuntime() {
     // == ! clearing canvas ! == \\
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+
+    // == ! Defining Game Logic for Winning/Losing ! == \\
+    if (detectHit(player, boss)) {
+        if (player.weapon) {
+            boss.alive = false
+            objective.display = false
+            wonTxt.display = true
+            // gameIsRunning = false
+        } else {
+            player.alive = false
+            // gameIsRunning = false
+            if (!player.alive) {
+                objective.display = false
+                hint.display = true
+                deathTxt.display = true
+            }
+        }
+
+    }
+    // == ! Game Logic for Interacting with Environments ! == \\
+    if (detectHit(player, wallStart)) {
+        characterMovement(-20)
+    } else {
+        characterMovement(10)
+    }
+    if (detectHit(player, wallEnd)) {
+        characterMovement(-20)
+    }
+    if (detectHit(player, wallConnector)) {
+        characterMovement(-20)
+    }
+    if (detectHit(player, wallBack)) {
+        characterMovement(-20)
+    }
+    if (detectHit(player, wallSide)) {
+        characterMovement(-20)
+    }
+    if (detectHit(player, wallEnd2)) {
+        characterMovement(-20)
+    }
+    if (detectHit(player, doorStart)) {
+        if (!player.hasKey) {
+            characterMovement(-20)
+        }
+    }
+    if (detectHit(player, swordChest)) {
+        if (!player.hasKey) {
+            characterMovement(-20)
+        }
+    }
+
+    if (detectHit(player, lavaTop)) {
+        player.alive = false
+        objective.display = false
+        lavaTxt.display = true
+        // gameIsRunning = false
+    }
+    if (detectHit(player, lavaBot)) {
+        player.alive = false
+        objective.display = false
+        lavaTxt.display = true
+        // gameIsRunning = false
+    }
+    if (detectHit(player, keyChest)) {
+        player.hasKey = true
+        keyChest.alive = false
+        console.log(player.hasKey)
+    }
+    if (detectHit(player, treasureChest)) {
+        player.weapon = true
+        treasureChest.alive = false
+        console.log(player.weapon)
+    }
+
+    // == ! Rendering of Objects ! == \\
+    floorMain.render()
+    wallStart.render()
+    wallBack.render()
+    wallSide.render()
+    wallConnector.render()
+    wallEnd.render()
+    wallEnd2.render()
+    lavaTop.render()
+    lavaBot.render()
+    walkway.render()
+    doorStart.render()
+    swordChest.render()
+    if (keyChest.alive) {
+        keyChest.render()
+    }
+    if (treasureChest.alive) {
+        treasureChest.render()
+    }
+    if (player.alive) {
+        player.render()
+    }
+    if (boss.alive) {
+        boss.render()
+    }
+
+    // == ! Rendering Text Elements ! == \\
+    if (objective.display) {
+        objective.render()
+    }
+    if (hint.display) {
+        hint.render()
+    }
+    if (deathTxt.display) {
+        deathTxt.render()
+    }
+    if (wonTxt.display) {
+        wonTxt.render()
+    }
+    if (swordTxt.display) {
+        swordTxt.render()
+    }
+    if (keyTxt.display) {
+        keyTxt.render()
+    }
+    if (lavaTxt.display) {
+        lavaTxt.render()
+    }
+    if (directTxt.display) {
+        directTxt.render()
+    }
+}
+
+function restart() {
+    // == ! Tried using reset() but didn't work ! == \\
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
 
@@ -300,6 +456,9 @@ function gameRuntime() {
     }
     if (lavaTxt.display) {
         lavaTxt.render()
+    }
+    if (directTxt.display) {
+        directTxt.render()
     }
 }
 
